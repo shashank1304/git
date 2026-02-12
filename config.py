1147a322c4593d3297d7615d4878a09b947a4c4d
yourusername <shashankkorram@gmail.com>
2026-02-12 23:17:39 +0530

    Testing secret detection

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-AWS_ACCESS_KEY_ID = "AKIA123456789EXAMPLE"
-AWS_SECRET_ACCESS_KEY = "abcd1234secretkeyexample"
+# Vulnerable Demo Code
+# DO NOT USE IN PRODUCTION
+
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+def connect_to_aws():
+    print("Connecting to AWS with hardcoded credentials...")
